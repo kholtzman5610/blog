3a922b6b984aa372308cb76d9234dfc2a829b3c9
vagrant <vagrant@vagrant.vm>
2020-04-13 19:29:52 +0000

    update

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,45 @@
+class PagesController < ApplicationController
+  before_action :set_page, only: [:show, :edit, :destory]
+
+  def index
+    @page = Page.all
+  end
+
+  def show
+  end
+
+  def new
+    @page = Page.new
+  end
+
+  def create
+    @page = Page.new(page_params)
+    @page.save
+    redirect_to @page
+  end
+
+  def edit
+    @page = Page.find(params[:id])
+  end
+
+  def update
+    @page = Page.find(params[:id])
+    @page.update(page_params)
+    redirect_to @page
+  end
+
+  def destroy
+    @page.destroy
+    redirect_to pages_path
+  end
+
+  private
+
+    def page_params
+      params.require(:page).permit(:title, :body, :slug)
+    end
+
+    def set_page
+      @page = Page.find(params[:id])
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200405142450) do
+ActiveRecord::Schema.define(version: 20200412214112) do
+
+  create_table "pages", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.string   "slug"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
