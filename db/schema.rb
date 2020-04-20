be490ca61ecbe4d7d955cc9df07369794e6d1c87
vagrant <vagrant@vagrant.vm>
2020-04-20 02:33:49 +0000

    blog master branch

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200419214408) do
+ActiveRecord::Schema.define(version: 20200420021244) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
     t.string   "name"
+    t.integer  "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "post_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
+  end
+
+  create_table "forums_users", id: false, force: :cascade do |t|
+    t.integer "user_id",  null: false
+    t.integer "forum_id", null: false
+    t.index ["forum_id", "user_id"], name: "index_forums_users_on_forum_id_and_user_id"
+    t.index ["user_id", "forum_id"], name: "index_forums_users_on_user_id_and_forum_id"
   end
 
   create_table "pages", force: :cascade do |t|
