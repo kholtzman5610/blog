3965cc396fadacfb3f918b0ba634b334f1c0dad1
vagrant <vagrant@vagrant.vm>
2020-05-07 00:47:26 +0000

    full-assignment update

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :customer, optional: true
+  validates_presence_of :customer
   validates :product_name, presence: true
   validates :product_count, presence: true
   validates :product_count, numericality: {only_integer: true}
